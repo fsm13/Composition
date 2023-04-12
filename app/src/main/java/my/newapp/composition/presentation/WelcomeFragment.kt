20a2587b248e3347b9a752a1a5fb618c8e842ae0
fsm13 <fomichevsm@gmail.com>
2023-04-12 15:51:35 +0700

    creating presentation module

---
+++ app/src/main/java/my/newapp/composition/presentation/WelcomeFragment.kt
@@ -0,0 +1,35 @@
+package my.newapp.composition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.newapp.composition.R
+import my.newapp.composition.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
