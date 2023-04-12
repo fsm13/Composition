8d5f56aabe36305d61edddd00625ed4ac0227b6b
fsm13 <fomichevsm@gmail.com>
2023-04-12 12:47:37 +0700

    creating domain module

--- app/src/main/java/my/newapp/composition/presentation/MainActivity.kt
+++ app/src/main/java/my/newapp/composition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package my.newapp.composition
+package my.newapp.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import my.newapp.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
